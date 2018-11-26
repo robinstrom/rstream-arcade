30beb8166d00651157a46bc35ce3718812a8d969
Robin Ström <robinstrom94@gmail.com>
2018-11-26 06:03:55 +0100

    setting up functional test for health check and updates to latest stable groovy version

--- functionaltests/src/test/groovy/com/rstreamdevelopments/arcade/health/HealthCheckSpec.groovy
+++ functionaltests/src/test/groovy/com/rstreamdevelopments/arcade/health/HealthCheckSpec.groovy
@@ -1,10 +1,19 @@
 package com.rstreamdevelopments.arcade.health
 
+import groovyx.net.http.RESTClient
 import spock.lang.Specification
 
 class HealthCheckSpec extends Specification {
 
-
     def "Health check should be up"() {
+        given:
+        def restClient = new RESTClient("http://localhost:8080/")
+
+        when:
+        def result = restClient.get(path: "/actuator/health")
+
+        then:
+        result.status == 200
+
     }
 }
